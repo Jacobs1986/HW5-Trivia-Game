590d7f4ca35b155b84ed1389e77c307536e92b33
Sean Jacobson <sean.jacobson86@gmail.com>
2020-02-05 16:19:54 -0700

    Added a div area for the quiz to and also put in some test questions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,15 @@
 console.log("linked")
+
+// Questions for the game
+let questions = [
+    {
+        question: "What is the answer to question 1?",
+        answers: ["a", "b", "c", "d"],
+        correct: "c"
+    },
+    {
+        question: "What is the answer to question 2?",
+        answers: ["A", "B", "C", "D"],
+        correct: "A"
+    }
+];
