b006be0fab52a3013d9004c4de9fa82c6b5aa1eb
Sean Jacobson <sean.jacobson86@gmail.com>
2020-02-05 16:08:22 -0700

    I decided to start from scratch with this. I want to make a better attempt at this game.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,1 @@
-$("#done").on("click", function () {
-    //I need to create a function that will check to see what is checked
-    console.log($("input:checked").val() + "is selected!");
-})
-
-
+console.log("linked")
