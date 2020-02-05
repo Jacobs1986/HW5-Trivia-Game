d8d92dd731efdcf5e243cde044a008c42a54cc82
Sean Jacobson <sean.jacobson86@gmail.com>
2020-02-05 16:52:30 -0700

    Added in something that will be able to check the answers. I took some of the code from the answer and tried to make it my own a bit. I had to change some variables. I'm doing my best not to fully rely on what was done, but I like the format.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 console.log("linked")
+let card = $("#quiz-area")
 
 // Questions for the game
 let questions = [
@@ -13,3 +14,39 @@ let questions = [
         correct: "A"
     }
 ];
+
+let game = {
+    start: function() {
+        console.log("clicked");
+        for (let i = 0; i < questions.length; i++) {
+            card.append(`<h2>${questions[i].question}</h2>`);
+            for (j = 0; j < questions[i].answers.length; j++) {
+                card.append(`<input type='radio' name='question-${i}' value='${questions[i].answers[j]}'>${questions[i].answers[j]}`);
+            }
+        }
+        $("#start").remove()
+        card.append("<br><br><button id='done'>Done</button>")
+    },
+    done: function() {
+        let input = card.children("input:checked");
+        for (let i = 0; i < input.length; i++) {
+            console.log(input);
+            if ($(input[i]).val() === questions[i].correct) {
+                console.log("You guessed right");
+            } else {
+                console.log("You guessed wrong.")
+            }
+        }
+
+        
+    }
+}
+
+$("#start").on("click", function(event) {
+    event.preventDefault();
+    game.start();
+})
+
+$(document).on("click", "#done", function() {
+    game.done();
+})
