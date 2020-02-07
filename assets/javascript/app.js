8a91514eebd5367c5bb9e45bf05230fd9790766d
Sean Jacobson <sean.jacobson86@gmail.com>
2020-02-06 20:04:36 -0700

    I made a function called loadQuestion that will load each question and it's answers. I had the function also create a button depending on the number of questions. If it is less than the number of questions then a next button appears. If it equals it then a done button appears.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,14 +16,36 @@ let questions = [
 ];
 let questionCount = 0;
 
+loadQuestion = () => {
+    console.log("Question will be loaded")
+    let questionText = questions[questionCount].question;
+    card.append(`<h2>${questionText}</h2>`);
+    // load the answers
+    let answers = questions[questionCount].answers;
+    answers.forEach(element => {
+        card.append(`<input type='radio' name='question-${questionText}' value='${element}'>${element}<br>`);
+    })
+    questionCount++
+    if (questionCount < 2) {
+        card.append("<br><button id='next'>Next</button>");
+    } else
+        card.append("<br><button>Done</button>")
+}
+
 
 
 // Click events
 $("#start").on("click", function(event) {
     event.preventDefault();
-    game.start();
+    $("#start").remove();
+    loadQuestion();
 })
 
 $(document).on("click", "#done", function() {
     game.done();
+})
+
+$(document).on("click", "#next", function() {
+    $(card).html("");
+    loadQuestion();
 })
